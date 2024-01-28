ff5ef7c4f2d23adcda37a62976cb89a7b9b2ff52
Abdul-Qudus <akindelequdus2@gmail.com>
2024-01-28 05:12:49 -0800

    Addition

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _printf: it is a function that prints formatted output.
+ * _printf - it is a function that prints formatted output.
  * @format: it is a pointer to a constant string.
  *
  * Return:the number of characters printed.
@@ -14,7 +14,6 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(args_lists, format);
 
 	while (*format != '\0')
@@ -27,31 +26,32 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
-			break;
-			if (*format == 'c')
+			switch (*format)
 			{
-				char c = va_arg(args_lists, int);
+				case 'c':
+					{
+						char c = va_arg(args_lists, int);
 
-				write(1, &c, 1);
-				printed_char++;
-			}
-			else if (*format == '%')
-			{
-				write(1, format, 1);
-				printed_char++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args_lists, char *);
+						write(1, &c, 1);
+						printed_char++;
+						break;
+					}
+				case ('s'):
+					{
+						char *s = va_arg(args_lists, char *);
 
-				write(1, &s, strlen(s));
-				printed_char += strlen;
+						write(1, &s, strlen(s));
+						printed_char += strlen;
+					}
+				case ('%'):
+						write(1, format, 1);
+						printed_char++;
+						break;
+				default:
+						break;
 			}
-		}
 		format++;
 	}
 		va_end(args_lists);
 		return (printed_char);
-
-
 }
