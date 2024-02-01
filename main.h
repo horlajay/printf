9e8fac2fb858a678b2944ae67c697fffb45696cc
cbdppk <122842339+cbdppk@users.noreply.github.com>
2024-02-01 01:58:04 -0600

    printf

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "_printf.c"
+
+int _printf(const char *format, ...);
+
+#endif
