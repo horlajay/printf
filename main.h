7c40dc8cd16db8d1a34c9ac883a4964b2e4c0993
danielk872 <juniordanielk872@gmail.com>
2024-07-18 17:55:01 -0500

    printf

--- main.h
+++ main.h
@@ -16,5 +16,6 @@ int parse_format(const char *format, va_list args);
 int _switch(const char *format, va_list args, int *i, int num_p);
 int handle_int(va_list args);
 int handle_negative_int(char *str);
+int handle_binary(va_list args);
 void reverse_string(char *str, int len);
 #endif
